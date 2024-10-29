5b0b1e9118ed9549adbf8d7915fc3ec51835be1c
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2024-10-29 21:19:05 +0530

    Create Question-1-3.cpp

---
+++ Sorting/Bubble Sort/Assignment/Question-1-3.cpp
@@ -0,0 +1,12 @@
+// Question 1 
+
+Ans:- 1,3 and 4 
+
+// Question 2 
+
+Ans:- 3rd option ([1,2,5,4,3,6])
+
+// Question 3 
+
+Ans :- Option 1 is correct (When the array is sorted in increasing order)
+
